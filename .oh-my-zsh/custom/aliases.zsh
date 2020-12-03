12d56b03945c1ab3904df53703affdc9994508ac
fugaz <davidefugazza@mac.com>
2020-12-03 11:35:58 +0100

    zsh support

---
+++ .oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,14 @@
+# useful aliases
+
+alias ll="ls -l"
+alias lo="ls -o"alias lh="ls -lh"
+alias la="ls -la"
+alias sl="ls"
+alias l="ls"
+alias s="ls"
+
+alias rm="rm -i"
+
+alias c="clear"
+
+export PATH="/usr/local/sbin:$PATH"
