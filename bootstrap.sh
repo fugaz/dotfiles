ea5811248f81db7ad781017bfe01f3fc4cf0c370
Davide Fugazza <fugazza@fugazza.com>
2020-12-07 08:43:19 +0100

    fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,8 +4,8 @@ cd "$(dirname "$0")"
 git pull
 function doIt() {
     rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
-    mv aliases.sh ~./.bash/
-    cp ~./.bash/aliases.sh ~./oh-my-zsh/custom
+    mv aliases.sh ~.bash/
+    cp ~.bash/aliases.sh ~.oh-my-zsh/custom
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
     doIt
